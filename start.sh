840683d4fa26e9ae85965015a45da361b1a530c0
shizukane <drizydarolle@gmail.com>
2020-06-17 12:30:50 +0300

    corrected functions

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='23'
 export MAIL_USERNAME='drizydarolle@gmail.com'
 export MAIL_PASSWORD='shizukane'
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
 
 python3 manage.py server
